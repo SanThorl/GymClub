4d0079ca5ee879f7930d710653566311b80fe4b9
ahthawm <santawmmaran@acedatasystems.com>
2025-02-12 16:17:01 +0630

    -

---
+++ GymClub.App/wwwroot/custom.js
@@ -0,0 +1,8 @@
+﻿window.enableLoading = function (isEnable) {
+    if (isEnable) {
+        document.getElementById('loading').style.display = "";
+    }
+    else {
+        document.getElementById('loading').style.display = "none";
+    }
+}
